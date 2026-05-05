50bf68c6d1751c00cd8d644c017f46b4bbaa1256
Svyatoslav Zakharov <kraldraav@gmail.com>
2026-05-05 14:31:15 +0300

    cli args, move client keys in external txt file

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const axios = require('axios');
+
+function createApp(clientKeys) {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use((req, res, next) => {
+    const key = req.headers['x-client-api-key'];
+    if (!key || !clientKeys.includes(key)) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    next();
+  });
+
+  app.post('/esia', async (req, res) => {
+    try {
+      const requestUrl = req.headers['x-target-url'];
+      if (!requestUrl) {
+        return res.status(400).json({ error: 'Missing X-Target-URL header' });
+      }
+
+      const response = await axios.post(
+        requestUrl,
+        req.body,
+        {
+          headers: {
+            Authorization: req.headers.authorization,
+            'Content-Type': 'application/json',
+            'Accept': '*/*'
+          }
+        }
+      );
+
+      res.status(response.status).json(response.data);
+    } catch (err) {
+      if (err.response) {
+        res.status(err.response.status).json(err.response.data);
+      } else {
+        res.status(500).json({ error: err.message });
+      }
+    }
+  });
+
+  return app;
+}
+
+module.exports = createApp;
